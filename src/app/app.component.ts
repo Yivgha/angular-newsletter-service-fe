0cc5e705cdde5a99b32c5e777495d66e7f13659f
Yivgha <genieukne@gmail.com>
2024-05-11 13:42:14 +0300

    add dynamic routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ContentComponent } from './content/content.component';
-import { FooterComponent } from './footer/footer.component';
-
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PostsComponent } from './posts/posts.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ContentComponent, FooterComponent],
   templateUrl: './app.component.html',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    PostsComponent,
+  ],
 })
 export class AppComponent {
-  title = 'newsletter-service-fe';
+  title = 'Service for posts';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: '**', component: ErrorPageComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
